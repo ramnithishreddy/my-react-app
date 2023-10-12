958611887bd9f9bf49b0755baf9b788793ab43c9
N Ram Nithish Reddy <ramnithishn@gmail.com>
2023-10-12 10:02:35 +0530

    This is the Footer of the Page

---
+++ src/Pages/Footer.js
@@ -0,0 +1,128 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer id="footer">
+        <div className="footer-newsletter">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-6">
+                <h4>Join Our Newsletter</h4>
+                <p>
+                  Tamen quem nulla quae legam multos aute sint culpa legam
+                  noster magna
+                </p>
+                <form action="" method="post">
+                  <input type="email" name="email" />
+                  <input type="submit" value="Subscribe" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 footer-contact">
+                <h3>TODOIST</h3>
+                <p>
+                  26/27 eshika building <br />
+                  Hyderabad, Telangana 535022 <br />
+                  India <br /><br />
+                  <strong>Phone:</strong> +91 63048 19271 <br />
+                  <strong>Email:</strong> ram@resourceone.com <br />
+                </p>
+              </div>
+
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right"></i> <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/Team">About us</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">Services</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">Terms of service</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">Privacy policy</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Our Services</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">Python</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i> <a href="/">Java</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">Angular</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">React</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">django</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Our Social Networks</h4>
+                <p>
+                  Cras fermentum odio eu feugiat lide par naso tierra videa
+                  magna derita valies
+                </p>
+                <div className="social-links mt-3">
+                  <a href="/" className="twitter">
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                  <a href="/" className="facebook">
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a href="/" className="instagram">
+                    <i className="bx bxl-instagram"></i>
+                  </a>
+                  <a href="/" className="google-plus">
+                    <i className="bx bxl-skype"></i>
+                  </a>
+                  <a href="/" className="linkedin">
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Typography variant="body2" color="text.secondary">
+          {"Copyright © "}
+          <span color="inherit" href="">
+            TODOIST
+          </span>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </footer>
+    </div>
+  );
+}
