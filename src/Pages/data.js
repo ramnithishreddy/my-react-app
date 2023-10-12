8e40dd62129f0f6fc8cfa703bd172ce105dc4c9f
N Ram Nithish Reddy <ramnithishn@gmail.com>
2023-10-12 09:54:48 +0530

    This my mock data

---
+++ src/Pages/data.js
@@ -0,0 +1,182 @@
+import team1 from "./assets/img/team/team-1.jpg";
+import team2 from "./assets/img/team/team-2.jpg";
+import team3 from "./assets/img/team/team-3.jpg";
+import team4 from "./assets/img/team/team-4.jpg";
+import portfolio1 from "./assets/img/portfolio/portfolio-1.jpg";
+import portfolio2 from "./assets/img/portfolio/portfolio-2.jpg";
+import portfolio3 from "./assets/img/portfolio/portfolio-3.jpg";
+import portfolio4 from "./assets/img/portfolio/portfolio-4.jpg";
+import portfolio5 from "./assets/img/portfolio/portfolio-5.jpg";
+import portfolio6 from "./assets/img/portfolio/portfolio-6.jpg";
+import portfolio7 from "./assets/img/portfolio/portfolio-7.jpg";
+import portfolio8 from "./assets/img/portfolio/portfolio-8.jpg";
+import portfolio10 from "./assets/img/portfolio/portfolio-10.jpg";
+import Capture from "./assets/img/Details/Capture.jpg";
+import Capture1 from "./assets/img/Details/Capture1.jpg";
+import Capture2 from "./assets/img/Details/Capture2.jpg";
+import Capture3 from "./assets/img/Details/Capture3.jpg";
+import Capture4 from "./assets/img/Details/Capture4.jpg";
+import Capture5 from "./assets/img/Details/Capture5.jpg";
+import Capture6 from "./assets/img/Details/Capture6.jpg";
+import Capture7 from "./assets/img/Details/Capture7.jpg";
+import Capture8 from "./assets/img/Details/Capture8.jpg";
+import Capture9 from "./assets/img/Details/Capture9.jpg";
+import Capture10 from "./assets/img/Details/Capture10.jpg";
+import Capture11 from "./assets/img/Details/Capture11.jpg";
+
+export const data = {
+  Team: [
+    {
+      team: 1,
+      Name: "Mayur Khandale",
+      Role: "Chief Executive Officer",
+      img: team1,
+      LinkContent: "https://mayurkhandale.github.io/education.html#graduation",
+      Link: "https://mayurkhandale.github.io/education.html#graduation",
+      about:
+        "Hey, Hi I am Mayur Khandale, I have completed my BCA graduate from N.Y.NC college and completed the course for Python, and UI development also did the project for the same. I have skills in Python, HTML, CSS, JavaScript, Bootstrap, Django.",
+    },
+    {
+      team: 2,
+      Name: "Aneesh Patil",
+      Role: "Product Manager",
+      img: team2,
+      LinkContent: "https://loopstudio-landing-page07.netlify.app/",
+      Link: "https://loopstudio-landing-page07.netlify.app/",
+      about:
+        "I am software enthusiast who likes to create stuff that helps people. Always up for fun and interesting projects. Interested in innovation and creativity.",
+    },
+    {
+      team: 3,
+      Name: "Naresh Baleboina",
+      Role: "CTO",
+      img: team3,
+      LinkContent: "https://nareshb7.github.io/",
+      Link: "https://nareshb7.github.io/",
+      about:
+        "Hey Hi this is Naresh, working as a React JS Developer and having 1 year of experience ,and also have the knowledge of the MERN stack. I developed a chat-application using MERN, socket.io",
+    },
+    {
+      team: 4,
+      Name: "Ram Nithish",
+      Role: "Accountant",
+      img: team4,
+      LinkContent:
+        "https://www.linkedin.com/pulse/travel-tourismskill-development-project-shaik-riyaz-basha?utm_source=share&utm_medium=member_android&utm_campaign=share_via",
+      Link: "https://www.linkedin.com/pulse/travel-tourismskill-development-project-shaik-riyaz-basha?utm_source=share&utm_medium=member_android&utm_campaign=share_via",
+      about:
+        " believe in hard work and believe that learning never ends so I try to learn new things and explore what I can do in my life. I would like to become an asset to the company I work for and hoping to gain as much as knowledge I can.",
+    },
+  ],
+  Portfolio: [
+    {
+      portfolio: 1,
+      Name: "MERN Stack",
+      img: portfolio1,
+    },
+    {
+      portfolio: 2,
+      Name: "Angular",
+      img: portfolio2,
+    },
+    {
+      portfolio: 3,
+      Name: "HTML,CSS,JavaScript",
+      img: portfolio3,
+    },
+    {
+      portfolio: 4,
+      Name: "React",
+      img: portfolio4,
+    },
+    {
+      portfolio: 5,
+      Name: "django",
+      img: portfolio5,
+    },
+    {
+      portfolio: 6,
+      Name: "Java",
+      img: portfolio6,
+    },
+    {
+      portfolio: 7,
+      Name: "Python",
+      img: portfolio7,
+    },
+    {
+      portfolio: 8,
+      Name: "BootStrap",
+      img: portfolio8,
+    },
+    {
+      portfolio: 9,
+      Name: "Redux",
+      img: portfolio10,
+    },
+  ],
+  Details: [
+    {
+      Project: "Sociomedia-MERN Stack",
+      Date: "07-09-2023",
+      img1: Capture,
+      img2: Capture11,
+      LinkContent: "https://socio-media-app.netlify.app/",
+      Link: "https://socio-media-app.netlify.app/",
+      about: "",
+      Tech: "MERN Stack",
+      Tech1: "Python",
+    },
+    {
+      Project: "chat application-Redux",
+      Date: "07-04-2023",
+      img1: Capture1,
+      img2: Capture3,
+      LinkContent: "https://simple-chatapplication.netlify.app/",
+      Link: "https://simple-chatapplication.netlify.app/",
+      about: "",
+      Tech: "Redux",
+      Tech1: "Java",
+    },
+    {
+      Project: "loopstudios-React,HTML,CSS",
+      Date: "07-10-2022",
+      img1: Capture2,
+      img2: Capture4,
+      LinkContent: "https://loopstudio-landing-page07.netlify.app/",
+      Link: "https://loopstudio-landing-page07.netlify.app/",
+      about: "",
+      Tech: "BootStrap",
+    },
+    {
+      Project: "Education Profile-HTML,CSS",
+      Date: "07-10-2020",
+      img1: Capture7,
+      img2: Capture8,
+      LinkContent: "https://mayurkhandale.github.io/education.html",
+      Link: "https://mayurkhandale.github.io/education.html",
+      about: "",
+      Tech: "HTML,CSS,JavaScript",
+    },
+    {
+      Project: "Resource Support Team-React,HTML,CSS,JavaScript,BootStrap",
+      Date: "07-08-2023",
+      img1: Capture9,
+      img2: Capture10,
+      LinkContent: "http://localhost:3000/",
+      Link: "http://localhost:3000/",
+      about: "",
+      Tech: "React",
+    },
+    {
+      Project: "Amazon-React,HTML,CSS,JavaScript,BootStrap",
+      Date: "07-07-2023",
+      img1: Capture5,
+      img2: Capture6,
+      LinkContent: "http://localhost:3001/",
+      Link: "http://localhost:3001/",
+      about: "",
+      Tech: "Angular",
+    },
+  ],
+};
